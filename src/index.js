b36c0d855087b26435cdefc03cc915d6d1ff9083
ajiprasetyo471 <ajiprsty4713@gmail.com>
2021-08-19 16:34:22 +0700

    melengkapi index.js menampilkan data di modal

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 $.ajax({
   url:
-    "https://api.themoviedb.org/3/search/movie?api_key=9b6e8f341d58e7535341bb35b8fac16f&query=godfather",
+    "https://api.themoviedb.org/3/search/movie?api_key=9b6e8f341d58e7535341bb35b8fac16f&query=avengers",
   success: (results) => {
     const movies = results.results;
-    console.log(movies);
     let cards = "";
     movies.forEach((m) => {
       cards += `<div class="col-md-4 my-3">
@@ -12,12 +11,65 @@ $.ajax({
                     <div class="card-body">
                       <h5 class="card-title">${m.title}</h5>
                       <h6 class="card-subtitle mb-2 text-muted">${m.release_date}</h6>
-                      <a href="#" class="btn btn-primary"  data-bs-toggle="modal" data-bs-target="#movieDetailModal">Show Details</a>
+                      <a href="#" class="btn btn-primary modal-detail-button"  data-bs-toggle="modal" data-bs-target="#movieDetailModal" data-id="${m.id}">Show Details</a>
                     </div>
                   </div>
                 </div>`;
     });
     $(".movie-container").html(cards);
+
+    // ketika tombol detail di-klik
+    $(".modal-detail-button").on("click", function () {
+      $.ajax({
+        url:
+          "https://api.themoviedb.org/3/movie/" +
+          $(this).data("id") +
+          "?api_key=9b6e8f341d58e7535341bb35b8fac16f",
+        success: (m) => {
+          const movieDetail = `<div class="container-fluid">
+                                  <div class="row">
+                                    <div class="col-md-3">
+                                      <img src="https://image.tmdb.org/t/p/original/${
+                                        m.poster_path
+                                      }" class="img-fluid" />
+                                    </div>
+                                    <div class="col-md">
+                                      <li class="list-group-item">
+                                        <h4>${m.title}</h4>
+                                      </li>
+                                      <li class="list-group-item">
+                                        <strong>Release Date : </strong>${
+                                          m.release_date
+                                        }</li>
+                                      <li class="list-group-item">
+                                        <strong>Genre : </strong>${m.genres.map(
+                                          (m) => m.name
+                                        )}</li>
+                                      <li class="list-group-item">
+                                        <strong>Country : </strong>${m.production_countries.map(
+                                          (m) => m.name
+                                        )}</li>
+                                      <li class="list-group-item"><strong>Rating : </strong>${
+                                        m.vote_average
+                                      }</li>
+                                      <li class="list-group-item">
+                                        <strong>Runtime : </strong>${
+                                          m.runtime
+                                        } min</li>
+                                      <li class="list-group-item">
+                                        <strong>Plot : </strong><br />${
+                                          m.overview
+                                        }</li>
+                                    </div>
+                                  </div>
+                                </div>`;
+          $(".modal-body").html(movieDetail);
+        },
+        error: (e) => {
+          console.log(e.responseText);
+        },
+      });
+    });
   },
   error: (e) => {
     console.log(e.responseText);
