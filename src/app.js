10b72f82f957cca47bb69284eb915ad06822d5ac
ajiprasetyo471 <ajiprsty4713@gmail.com>
2021-08-26 16:36:46 +0700

    membuat konfigurasi webpack dan sudah bundle

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import './styles/style.css';
+import './script/component/nav-bar.js';
+import './script/component/search-bar.js';
 import {
     main
 } from './script/view/main.js';
-import './script/component/nav-bar.js';
-import './script/component/search-bar.js';
 
 document.addEventListener("DOMContentLoaded", main);
